f54de0aceac4d04112f0bf9cb8e4791f4cfc5420
asglasby <aisha.developer@gmail.com>
2015-04-14 13:37:37 -0400

    code clean up

--- JSDay1ContactList/main.js
+++ JSDay1ContactList/main.js
@@ -33,22 +33,7 @@ var deleteContact = function (i) {
     listContacts();
 }
 
-//var updateContact = function (i) {
-//    // alert('not working yet');
-//    var nameId = "name" + i;
-//    var emailId = "email" + i;
-//    var phoneId = "phone" + i;
-
-//    document.getElementById(nameId).innerHTML = '<input value="' + contacts[i].name + '" class="form-control" id="name' + i + '" />';
-//    document.getElementById(emailId).innerHTML = '<input value="' + contacts[i].email + '" class="form-control" id="email' + i + '" />';
-//    document.getElementById(phoneId).innerHTML = '<input value="' + contacts[i].phone + '" class="form-control" id="phone' + i + '" /><br /><button id="updateButton" class="btn btn-warning" onclick=submitUpdatedContact(' + i + ')>Submit</button>';
-//}
-
 var updateContact = function (i) {
-    // alert('not working yet');
-    //var nameId = "name" + i;
-    //var emailId = "email" + i;
-    //var phoneId = "phone" + i;
     contactId = i;
     document.getElementById("inputName").value = contacts[i].name;
     document.getElementById("inputEmail").value = contacts[i].email;
@@ -57,25 +42,7 @@ var updateContact = function (i) {
    
 }
 
-//var submitUpdatedContact = function (i) {
-//    // alert("not working just yet")
-//    //var nameUId = "name" + i;
-//    //var emailUId = "email" + i;
-//    //var phoneUId = "phone" + i;
-
-//    contacts[i].name = document.getElementById(inputName).value;
-//    contacts[i].email = document.getElementById(emailUId).value;
-//    contacts[i].phone = document.getElementById(phoneUId).value;
-
-//    listContacts();
-//}
-
 var submitUpdatedContact = function (i) {
-    // alert("not working just yet")
-    //var nameUId = "name" + i;
-    //var emailUId = "email" + i;
-    //var phoneUId = "phone" + i;
-
     contacts[i].name = document.getElementById("inputName").value;
     contacts[i].email = document.getElementById("inputEmail").value;
     contacts[i].phone = document.getElementById("inputPhone").value;
